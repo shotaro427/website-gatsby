4ff1ca0e04aba9663ec645a3e56558317ca6a8f8
shotaro427 <toshinobu.s111@gmail.com>
2022-02-23 17:24:19 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+/** @type {import('gatsby).GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+      title: `new`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "",
+      "spaceId": ""
+    }
+  }, "gatsby-plugin-emotion", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
